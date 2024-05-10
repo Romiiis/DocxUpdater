eac9ba928d362598764a05a9cb65055cc256355a
Roman Pejs <romispejs@gmail.com>
2024-05-10 15:25:06 +0200

    Changed structure of code
    Three for loops are now in one for loop

--- docxUpdater.py
+++ docxUpdater.py
@@ -1,76 +1,61 @@
-#import python-docx
+# Imports
 from docx import Document
 import re
 from tkinter import filedialog, messagebox
 
+# Prefixes for the print statements
 prefix_info = "[INFO] "
 prefix_error = "[ERROR] "
 
-# Create small GUI to select the docx file
+# Dialog box to select the docx file
 filename = filedialog.askopenfilename()
 
-#if filename is not docx file, exit the program
+# If the filename is empty, show an error message and exit the program or if the file is not a docx file
 if not filename.endswith('.docx'):
     messagebox.showerror('Error', 'Please select a docx file')
     print(prefix_error + 'Please select a docx file')
+    input('Press Enter to close the program')
     exit()
 
-#open the docx file
+# Create a Document object from the docx file
 doc = Document(filename)
 
+# Find the first year in the docx file and store it in the variable year
 year = None
-
-#get first year in the docx file and print it
 for para in doc.paragraphs:
     if re.search(r'\b\d{4}\b', para.text):
         year = re.search(r'\b\d{4}\b', para.text).group()
         break
 
-#find also in tables and replace with the next year
+# Find the year in the table and replace it with the next year
 for table in doc.tables:
     for row in table.rows:
         for cell in row.cells:
             if re.search(r'\b\d{4}\b', cell.text):
                 for match in re.finditer(r'\b\d{4}\b', cell.text):
                     if match.group() == year:
-                        cell.text = cell.text.replace(year, str(int(year)+1))
+                        cell.text = cell.text.replace(year, str(int(year) + 1))
 
+# Find the year in the paragraphs and replace it with the next year
+for i in range(1, -2, -1):
+    for para in doc.paragraphs:
+        if re.search(r'\b\d{4}\b', para.text):
+            for match in re.finditer(r'\b\d{4}\b', para.text):
+                if match.group() == str(int(year) + i):
+                    para.text = para.text.replace(str(int(year) + i), str(int(year) + i + 1))
 
-#find all years like the found year+1 and replace them with the next year
-for para in doc.paragraphs:
-    if re.search(r'\b\d{4}\b', para.text):
-        for match in re.finditer(r'\b\d{4}\b', para.text):
-            if match.group() == str(int(year)+1):
-                para.text = para.text.replace(str(int(year)+1), str(int(year)+2))
+# Find the year in the filename and replace it with the next year
+filename = filename.replace(year, str(int(year) + 1))
 
+# Save the updated docx file
+doc.save(filename.split('/')[-1])
 
-#find all years like the found year and replace them with the next year
-for para in doc.paragraphs:
-    if re.search(r'\b\d{4}\b', para.text):
-        for match in re.finditer(r'\b\d{4}\b', para.text):
-            if match.group() == year:
-                # higlight the change in the docx filr
-                para.text = para.text.replace(year, str(int(year)+1))
+# Dialog box to show the file is updated and saved
+messagebox.showinfo('Info', 'File updated and saved as ' + filename.split('/')[-1])
 
+# Print the file is updated and saved and the path of the updated file (absolute path)
+print(prefix_info + 'File updated and saved as ' + filename.split('/')[-1])
+print(prefix_info + 'Path: ' + filename + '\n')
 
-
-
-#find all years like the found year-1 and replace them with the next year
-for para in doc.paragraphs:
-    if re.search(r'\b\d{4}\b', para.text):
-        for match in re.finditer(r'\b\d{4}\b', para.text):
-            if match.group() == str(int(year)-1):
-                para.text = para.text.replace(str(int(year)-1), year)
-
-#save the docx file with the updated years
-doc.save('updated_'+filename.split('/')[-1])
-
-#dialog box to show the file is updated and saved and where it is saved
-messagebox.showinfo('Info', 'File updated and saved as updated_'+filename.split('/')[-1])
-
-print(prefix_info + 'File updated and saved as updated_'+filename.split('/')[-1])
-
-#dont close the program
+# Wait for enter key to close the program
 input('Press Enter to close the program')
-
-
