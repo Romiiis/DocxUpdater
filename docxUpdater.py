4697e494b5e7365db06b6db731dd9f49a1144fd1
Roman Pejs <romispejs@gmail.com>
2024-05-16 21:05:15 +0200

    Handling errors
    Two attempts to choose correct file -> then error and program ends.
    
    If any error occoured, error message + log in console.

--- docxUpdater.py
+++ docxUpdater.py
@@ -8,54 +8,69 @@ prefix_info = "[INFO] "
 prefix_error = "[ERROR] "
 
 # Dialog box to select the docx file
-filename = filedialog.askopenfilename()
+filename = ""
 
-# If the filename is empty, show an error message and exit the program or if the file is not a docx file
-if not filename.endswith('.docx'):
-    messagebox.showerror('Error', 'Please select a docx file')
-    print(prefix_error + 'Please select a docx file')
-    input('Press Enter to close the program')
-    exit()
+# If the filename is empty, show an error message and exit the program or if the file is not a docx file (try 2 times)
+for i in range(2):
 
-# Create a Document object from the docx file
-doc = Document(filename)
+    # Open the file dialog box to select the docx file
+    filename = filedialog.askopenfilename()
 
-# Find the first year in the docx file and store it in the variable year
-year = None
-for para in doc.paragraphs:
-    if re.search(r'\b\d{4}\b', para.text):
-        year = re.search(r'\b\d{4}\b', para.text).group()
+    # If the filename is empty, show an error message and exit the program
+    if filename == '':
+        messagebox.showerror('Error', 'No file selected \nPlease select a .docx file')
+    # If the file is not a docx file, show an error message and ask to select a docx file
+    elif not filename.endswith('.docx'):
+        messagebox.showerror('Error', 'Please select a docx file')
+    # If the file is a docx file, break the loop
+    else:
         break
 
-# Find the year in the table and replace it with the next year
-for table in doc.tables:
-    for row in table.rows:
-        for cell in row.cells:
-            if re.search(r'\b\d{4}\b', cell.text):
-                for match in re.finditer(r'\b\d{4}\b', cell.text):
-                    if match.group() == year:
-                        cell.text = cell.text.replace(year, str(int(year) + 1))
+# If the filename is empty, show an error message and exit the program
+if filename == "":
+    messagebox.showerror('Error', 'Operation cancelled')
+    exit()
 
-# Find the year in the paragraphs and replace it with the next year
-for i in range(1, -2, -1):
+try:
+    # Create a Document object from the docx file
+    doc = Document(filename)
+
+    # Find the first year in the docx file and store it in the variable year
+    year = None
     for para in doc.paragraphs:
         if re.search(r'\b\d{4}\b', para.text):
-            for match in re.finditer(r'\b\d{4}\b', para.text):
-                if match.group() == str(int(year) + i):
-                    para.text = para.text.replace(str(int(year) + i), str(int(year) + i + 1))
+            year = re.search(r'\b\d{4}\b', para.text).group()
+            break
 
-# Find the year in the filename and replace it with the next year
-filename = filename.replace(year, str(int(year) + 1))
+    # Find the year in the table and replace it with the next year
+    for table in doc.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                if re.search(r'\b\d{4}\b', cell.text):
+                    for match in re.finditer(r'\b\d{4}\b', cell.text):
+                        if match.group() == year:
+                            cell.text = cell.text.replace(year, str(int(year) + 1))
 
-# Save the updated docx file
-doc.save(filename.split('/')[-1])
+    # Find the year in the paragraphs and replace it with the next year
+    for i in range(1, -2, -1):
+        for para in doc.paragraphs:
+            if re.search(r'\b\d{4}\b', para.text):
+                for match in re.finditer(r'\b\d{4}\b', para.text):
+                    if match.group() == str(int(year) + i):
+                        para.text = para.text.replace(str(int(year) + i), str(int(year) + i + 1))
 
-# Dialog box to show the file is updated and saved
-messagebox.showinfo('Info', 'File updated and saved as ' + filename.split('/')[-1])
+    # Find the year in the filename and replace it with the next year
+    filename = filename.replace(year, str(int(year) + 1))
 
-# Print the file is updated and saved and the path of the updated file (absolute path)
-print(prefix_info + 'File updated and saved as ' + filename.split('/')[-1])
-print(prefix_info + 'Path: ' + filename + '\n')
+    # Save the updated docx file
+    doc.save(filename.split('/')[-1])
 
-# Wait for enter key to close the program
-input('Press Enter to close the program')
+    # Dialog box to show the file is updated and saved
+    messagebox.showinfo('Info', 'File updated and saved as ' + filename.split('/')[-1])
+
+except Exception as e:
+    # If any error occurs, show an error message and exit the program
+    print("Error: " + str(e))
+    messagebox.showerror('Error', 'An error occurred \nPlease try again or contact the developer')
+    input("Press ENTER key to exit")
+    exit()
